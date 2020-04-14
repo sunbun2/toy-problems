bf10a79d4f40b9d994c8ca01752d79aafdd5ff04
Abhiram Rayala <55695264+sunbun2@users.noreply.github.com>
2020-04-14 07:00:05 +0530

    defining methods

--- lru-cache.py
+++ lru-cache.py
@@ -1,5 +1,14 @@
 class LRU:
+    
     def __init__(self,c):
         self.c=c
         self.list=[]
         self.inputs={}
+    
+    def put(self,key,value):
+        pass
+
+    def get(self,key):
+        pass
+    def get_cache(self):
+        pass

6b6060c821ac1a212236c6945e2471cff0d62080
Abhiram Rayala <55695264+sunbun2@users.noreply.github.com>
2020-04-16 09:43:13 +0530

    testcases

--- test_lru.py
+++ test_lru.py
@@ -1,10 +1,61 @@
 import unittest
 from lrucache import *
 
-class Testcases(unittest.TestCase):
+class test_lru(unittest.TestCase):
 
-    def test_lru_get(self):
-        l = ['one','two','three','four','five']
-        for i in range(1,6):
-            put(i,l[i-1])
+    def test_get(self):
+        lru = LRU(5)
+        for i in range(5):
+            lru.put(i,'sample '+str(i))
+        for i in range(5):
+            self.assertEqual(lru.get(i),'sample '+str(i))
+        pass
         
+    def test_get2(self):
+        lru = LRU(5)
+        self.assertEqual(lru.get(1),-1)
+        self.assertEqual(lru.get(-1),-1)
+        pass
+        
+    
+    def test_get3(self):
+        lru = LRU(3)
+
+        for  i in range(100):
+            lru.put(i,'text '+str(i))
+        self.assertEqual(lru.get(0),-1)
+        self.assertEqual(lru.get(97),'text 97')
+        pass
+
+    def test_get_cache(self):
+        lru = LRU(5)
+        for i in range(5):
+             lru.put(i,'sample '+str(i))
+        self.assertEqual(lru.get_cache(),'[0, 1, 2, 3, 4]')
+        pass
+
+    def test_get_cache2(self):
+        lru = LRU(5)
+        self.assertEqual(lru.get_cache(),'[]')
+        pass
+
+    def test_get_cache3(self):
+        lru=LRU(1)
+        for i in range(5):
+            lru.put(i,'sample '+str(i))
+        self.assertEqual(lru.get_cache(),'[4]')
+        pass
+
+    def test_put1(self):
+        lru = LRU(5)
+        lru.put(1,'sample test')
+        self.assertEqual(lru.get(1),'sample test')
+
+    def test_put2(self):
+        lru = LRU(5)
+        lru.put(1,-1)
+        self.assertEqual(lru.get(1),-1)
+
+
+if __name__ == '__main__':
+    unittest.main()

631b84b05234c3be45bf7e850ccf1fcd552b3b74
Abhiram Rayala <55695264+sunbun2@users.noreply.github.com>
2020-04-16 09:19:40 +0530

    last

--- lrucache.py
+++ lrucache.py
@@ -4,11 +4,28 @@ class LRU:
         self.c=c
         self.list=[]
         self.inputs={}
+        pass
     
     def put(self,key,value):
+        if (key in self.list):
+            self.list.pop(self.list.index(key))
+            self.list.append(key)
+            self.inputs[key]=value
+        else:
+            if (len(self.list) == self.c):
+                temp = self.list.pop(0)
+                del(self.inputs[temp])
+            self.list.append(key)
+            self.inputs[key]= value
+
         pass
 
     def get(self,key):
-        pass
+        if key in self.inputs:
+            return self.inputs[key]
+            self.list.append(key)
+        return -1
+
     def get_cache(self):
+        return str(self.list)
         pass

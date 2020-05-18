b4468c75a7bde9d3357ee5ed6609e3ac0199319d
Abhiram Rayala <55695264+sunbun2@users.noreply.github.com>
2020-05-18 10:18:10 +0530

    designed and written a class called Todolist which has mthods which perform task like addtask,delete task, handling change

--- react-todo/todo-app.js
+++ react-todo/todo-app.js
@@ -21,3 +21,56 @@ function Task(props) {
     
     </li>
 }
+
+
+class TodoList extends React.Component {
+    constructor(props) {
+        console.log("todolist")
+        super(props);
+        this.state = {list: props.list};
+
+        this.handleAddTask = this.handleAddTask.bind(this);
+    }
+    handleAddTask(task) {
+        console.log("add task clicked");
+        this.state.list.push(task);
+        this.setState({list: this.state.list})
+    }
+    handleDeleteTask = (taskId) => {
+		console.log('delete button clicked');
+		const list = this.state.list.filter((t) => t.id !== taskId);
+		this.setState({ list: list });
+    };
+    
+    handleChange = (taskId) => {
+		console.log('checkbox clicked');
+		this.setState({
+			list: this.state.list.map((task) => {
+				if (task.id === taskId) {
+					return {
+						...task,
+						status: !task.status
+					};
+				} 
+				return task;
+				
+			})
+		});
+	};
+
+    render() {
+        console.log("todolist render")
+        return (
+            <div>
+                <h1>TODO List</h1>
+                <ol>
+                    {
+                        this.state.list.map((t) =>
+                            <Task key={t.id} name={t.name} dueDate={t.dueDate} onDelete={this.handleDeleteTask} onChange = {this.handleChange}task = {t} />)
+                    }
+                </ol>
+                <TaskNameForm onAddTask={this.handleAddTask} />
+            </div>
+        );
+    }
+}
